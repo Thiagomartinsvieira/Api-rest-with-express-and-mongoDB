d90080fb2f4eac5ef01acbcf9f9c74defb3a980a
Thiago <thiago.devstack@gmail.com>
2025-04-18 17:38:53 -0300

    feat: creating server

--- express-mongo/server.js
+++ express-mongo/server.js
@@ -1,1 +1,13 @@
 import http from "http";
+
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "content-type": "text/plain" })
+    res.end("Node.Js is running")
+})
+
+server.listen(PORT, () => {
+    console.log("Server is Listening!")
+})
+
