62ed0da12afb8e14c579a77c2b50f4f9fa1bad57
Thiago <thiago.devstack@gmail.com>
2025-04-18 18:50:50 -0300

    feat: initializing express

--- express-mongo/server.js
+++ express-mongo/server.js
@@ -1,19 +1,8 @@
-import http from "http";
+import app from "./src/app.js"
 
 const PORT = 3000
 
-const routes = {
-    "/": "Express API",
-    "/books": "Route Books",
-    "/authors": "Route authors"
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/plain" })
-    res.end(routes[req.url])
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Server is Listening!")
 })
 
