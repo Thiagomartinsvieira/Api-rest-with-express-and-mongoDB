72d53badcd33ab584158654c4a3b81126519bd40
Thiago <thiago.devstack@gmail.com>
2025-04-19 10:32:27 -0300

    feat: creating the books route

--- express-mongo/src/app.js
+++ express-mongo/src/app.js
@@ -2,8 +2,23 @@ import express from "express"
 
 const app = express();
 
+const books = [
+    {
+        id: 1,
+        tittle: "The lord of the rings"
+    },
+    {
+        id: 2,
+        title: "The Hobbit"
+    }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("Node.js with express")
 })
+
+app.get("/books",(req, res) => {
+    res.status(200).json(books);
+});
 
 export default app;
